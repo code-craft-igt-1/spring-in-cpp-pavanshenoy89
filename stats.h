5725e436f04cd16bc7706efe07ac7d1cc94b9b1b
pavanshenoy89 <71321360+pavanshenoy89@users.noreply.github.com>
2024-07-25 16:03:44 +0530

    Define ComputeStatistics and update its test cases
    Define ComputeStatistics and update its test cases

--- stats.h
+++ stats.h
@@ -1,7 +1,18 @@
 #include <vector>
 
 namespace Statistics {
-    // define the Stats structure here. See the tests to infer its properties
     
-    Stats ComputeStatistics(const std::vector<___>& );
+
+    /**
+     * @brief Represents statistical information.
+     * 
+     * This struct stores the maximum, minimum, and average values of a dataset.
+     */
+    struct Stats {
+        float max;      // maximum value
+        float min;      // minimum value
+        float average;  // average value
+    };
+    
+    Stats ComputeStatistics(const std::vector<float>& );
 }
